429997a58f4cf2e313fc2cb2bb3d96dee68478a0
shunting <shunting314@gmail.com>
2022-07-27 23:47:41 -0700

    multi line comment

--- sample_input.c
+++ sample_input.c
@@ -1,4 +1,19 @@
 {
-  double d;
-  d = 0.5;
+  double a[100][100];
+  double b[100][100];
+  double c[100][100];
+  int i;
+  int j;
+  int k;
+  int n;
+  n = 100;
+  /*
+  for (i = 0; i < n; i++)
+    for (j = 0; j < n; j++)
+      c[i][j] = 0.0;
+  for (i = 0; i < n; i++)
+    for (j = 0; j < n; j++)
+      for (k = 0; k < n; k++)
+        c[i][j] = c[i][j] + a[i][k] * b[k][j];
+  */
 }
